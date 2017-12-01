25ff6b264348020a9a6de1789571b67913c5e224
Rollin Metzger <rollin.metzger@gmail.com>
2017-12-01 19:50:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ function sayHiToGrandma(string) {
   if (string.toUpperCase) {
     return "YES INDEED!"
   }
-  if (string.toLowerCase) {
+  else if (string.toLowerCase) {
     return "I can't hear you!"
   }
-  if (string === "I love you, Grandma") {
+  else if (string === "I love you, Grandma") {
     return "I love you, too."
   }
 }
