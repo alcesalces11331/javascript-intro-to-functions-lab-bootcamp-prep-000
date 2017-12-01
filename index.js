5e721f57c48977804d9391c678a055bc8b614c7b
Rollin Metzger <rollin.metzger@gmail.com>
2017-12-01 19:53:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,11 +17,9 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toUpperCase) {
     return "YES INDEED!"
-  }
-  else if (string.toLowerCase) {
+  } else if (string.toLowerCase) {
     return "I can't hear you!"
-  }
-  else if (string === "I love you, Grandma") {
+  } else if (string === "I love you, Grandma") {
     return "I love you, too."
   }
 }
